fe6b41c0c550da767e1835183460f738d81041ab
ravi mehra <ravi.lib17@gmail.com>
2014-06-13 13:07:20 +0530

    rspec guard spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-
 describe "Static pages" do
+let{:base_title} {"Ruby on Rails Tutorial Sample App"} 
 
   describe "Home page" do
 
@@ -10,7 +10,7 @@ describe "Static pages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   describe "Help page" do
@@ -21,7 +21,7 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -32,19 +32,19 @@ describe "Static pages" do
     end
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact us'" do
+    it "should have the content 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
+      expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
